effd951b087aed5e4d2fe175fdb73f363987055d
Alan Calderon Castro <alacalcasis@gmail.com>
2015-10-14 18:15:50 +0000

    Se prueba thread de c++11.

--- BibGrafos/main.cpp
+++ BibGrafos/main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <thread>
 using namespace std;
 
 #include "Grafo.h"
@@ -22,6 +23,16 @@ int main(int argc, char** argv) {
     cout << grfInt.aHil() << endl;
     if (grfInt.ciclos(0,salida))
         cout << "sí encontró un ciclo: " << salida << endl;
+    std::thread first (foo);     // spawn new thread that calls foo()
+    std::thread second (bar,0);  // spawn new thread that calls bar(0)
+
+    std::cout << "main, foo and bar now execute concurrently...\n";
+
+    // synchronize threads:
+    first.join();                // pauses until first finishes
+    second.join();               // pauses until second finishes
+
+    std::cout << "foo and bar completed.\n";        
     return 0;
 }
 
