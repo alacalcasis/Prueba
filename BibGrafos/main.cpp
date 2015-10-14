ba675d3a136a3672d0f0d3a5545335f1eb4d51ae
Alan Calderon Castro <alacalcasis@gmail.com>
2015-10-14 18:06:15 +0000

    Segundo commit que agrega archivos de prueba de proyecto grafos.

---
+++ BibGrafos/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: alan.calderon
+ *
+ * Created on 25 de septiembre de 2015, 05:16 PM
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "Grafo.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    string nombre_archivo = "grafoMuyPeq.txt";
+    string salida;
+    Grafo<int> grfInt(nombre_archivo);
+    cout << grfInt.aHil() << endl;
+    if (grfInt.ciclos(0,salida))
+        cout << "sí encontró un ciclo: " << salida << endl;
+    return 0;
+}
+
